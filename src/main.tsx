5114cc50f0bfa3664a70f9f67cc9456c523c2e87
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2020-10-14 10:58:00 +0200

    Add basic tree select

--- src/main.tsx
+++ src/main.tsx
@@ -3,31 +3,47 @@
  * Create or update a factory graph to produce a set of elements.
  * lgfrbcsgo & Nikolaus - October 2020
  */
-import { items } from "./items"
-import { buildFactory } from "./factory"
+import { Item, items } from "./items"
 import * as React from "react"
+import { useMemo, useState } from "react"
 import * as ReactDOM from "react-dom"
-
-Object.assign(window, {
-    items,
-    buildFactory,
-})
-
-console.log(
-    `
-// produce each item with two assemblers
-const requirements = new Map([
-    [items["Adjustor L"], 2],
-    [items["Vertical Booster L"], 2],
-    [items["Retro-Rocket Brake L"], 2],
-])
-const factory = buildFactory(requirements)
-`,
-    "color: #000099",
-)
+import { TreeSelect } from "antd"
+import { compose, groupBy, toPairs, values } from "ramda"
+import "antd/dist/antd.css"
 
 function App() {
-    return <h1>Open the Javascript console</h1>
+    const [selection, setSelection] = useState<Item[]>([])
+
+    const treeData = useMemo(() => {
+        const groupByCategory = compose(
+            toPairs,
+            groupBy((item: Item) => item.category),
+        )
+        const grouped = groupByCategory(values(items))
+        return grouped.map(([category, items]) => ({
+            title: category,
+            value: category,
+            children: items.map((item) => ({
+                title: item.name,
+                value: item.name,
+            })),
+        }))
+    }, [items])
+
+    return (
+        <TreeSelect
+            style={{ width: "100%" }}
+            value={selection}
+            dropdownStyle={{ maxHeight: 400, overflow: "auto" }}
+            treeData={treeData}
+            placeholder="Please select"
+            multiple
+            allowClear
+            showArrow
+            treeCheckable
+            onChange={setSelection}
+        />
+    )
 }
 
 const rootElement = document.getElementById("root")
