dac052cedb97f74392b32f51e8724b7398cb9cdc
Trey Wenger <tvwenger@gmail.com>
2020-10-17 11:16:13 -0700

    initial jointjs implementation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -5,6 +5,8 @@ import { values } from "ramda"
 import { ItemSelect } from "./item-select"
 import { Button, Col, InputNumber, Row } from "antd"
 import { buildFactory } from "../factory"
+import { FactoryGraph } from "../graph"
+var joint = require("jointjs")
 
 /**
  * Root component of the app
@@ -58,11 +60,70 @@ export function App() {
                     </Col>
                 </Row>
             ))}
-            <Button type="primary" onClick={() => console.log(buildFactory(getRequirements()))}>
+            <Button type="primary" onClick={() => Graph(buildFactory(getRequirements()))}>
                 Generate
             </Button>
         </Fragment>
     )
+}
+
+/**
+ * Visualize factory graph
+ */
+export function Graph(factory: FactoryGraph) {
+    console.log("Build factory")
+    var graph = new joint.dia.Graph()
+    var el = document.getElementById("graph")!
+    var paper = new joint.dia.Paper({
+        el: el,
+        model: graph,
+        width: el.clientWidth,
+        height: el.clientHeight,
+        gridSize: 1,
+        interactive: false,
+    })
+    var isDragged = false
+    var dragStartPosition = { x: 0, y: 0 }
+    console.log(dragStartPosition)
+    paper.on("blank:pointerdown", function (event: MouseEvent, x: number, y: number) {
+        isDragged = true
+        dragStartPosition = { x: x, y: y }
+    })
+    paper.on("cell:pointerup blank:pointerup", function (cellView: any, x: number, y: number) {
+        isDragged = false
+    })
+    el.addEventListener("mousemove", function (event: MouseEvent) {
+        if (isDragged)
+            paper.translate(
+                event.offsetX - dragStartPosition.x,
+                event.offsetY - dragStartPosition.y,
+            )
+    })
+    var x = 10
+    var y = 10
+    /** Loop over containers */
+    for (const container of factory.containers) {
+        /* Add container */
+        var containerRect = new joint.shapes.standard.Rectangle({
+            position: { x: x, y: y },
+            size: { width: 50, height: 50 },
+        })
+        containerRect.attr({
+            body: {
+                fill: "white",
+            },
+            label: {
+                text: container.item.name,
+                fill: "blac",
+            },
+        })
+        containerRect.addTo(graph)
+        y += 60
+        if (y > 600) {
+            y = 10
+            x += 60
+        }
+    }
 }
 
 /**
