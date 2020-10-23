526633908d47ce18a9744e1edc91bfd42bc579a8
Trey Wenger <tvwenger@gmail.com>
2020-10-23 08:29:11 -0700

    move handleIndustryLinks and sanityCheck to standalone function

--- src/factory.ts
+++ src/factory.ts
@@ -11,8 +11,10 @@ import {
     FactoryGraph,
     PerMinute,
     TransferNode,
-    OutputNode,
+    TransferContainerNode,
+    isTransferContainerNode,
     MAX_CONTAINER_LINKS,
+    MAX_INDUSTRY_LINKS,
 } from "./graph"
 
 /**
@@ -148,6 +150,127 @@ function handleByproducts(factory: FactoryGraph) {
 }
 
 /**
+ * Add transfer units and containers to handle industries that require
+ * >7 incoming links
+ * @param factory the FactoryGraph
+ */
+function handleIndustryLinks(factory: FactoryGraph): void {
+    // Loop over all factory industries
+    for (const industry of factory.industries) {
+        // Get ingredients and ingredient quantities
+        const recipe = findRecipe(industry.item)
+        // Sort ingredients by quantity
+        const recipeIngredients = recipe.ingredients
+        recipeIngredients.sort((a, b) => a.quantity - b.quantity)
+        const ingredients = recipeIngredients.map((ingredient) => ingredient.item)
+
+        // Get exceeding link count
+        const exceedingLinks = industry.incomingLinkCount - MAX_INDUSTRY_LINKS
+        if (exceedingLinks > 0) {
+            let transferContainer: TransferContainerNode | undefined
+
+            // Get all transfer containers containing a subset of the industry ingredients
+            const transferContainers = factory.getTransferContainers(new Set(ingredients))
+            for (const checkTransferContainer of transferContainers) {
+                // Check if this transfer container has at least exceedingLinks+1 items
+                // +1 because we need to remove one link to make space for TransferContainerNode
+                if (checkTransferContainer.items.length < exceedingLinks + 1) {
+                    continue
+                }
+                // Check if we can add an outgoing link from this TransferContainerNode
+                if (!checkTransferContainer.canAddOutgoingLinks(1)) {
+                    continue
+                }
+                // Check if we can add one ingoing link to each transfer unit on this TransferContainerNode
+                if (
+                    Array.from(checkTransferContainer.producers).some(
+                        (node) => !node.canAddIncomingLinks(1),
+                    )
+                ) {
+                    continue
+                }
+                // good
+                transferContainer = checkTransferContainer
+                break
+            }
+
+            // Create a new TransferContainerNode if necessary
+            if (transferContainer === undefined) {
+                // Use first exceedingLinks+1 items in ingredients (sorted by quantity)
+                // +1 because we need to remove one link to make space for TransferContainerNode
+                const items = ingredients.slice(0, exceedingLinks + 1)
+                transferContainer = factory.createTransferContainer(items)
+                // Add transfer units
+                for (const item of items) {
+                    const transferUnit = factory.createTransferUnit(item)
+                    transferUnit.outputTo(transferContainer)
+                }
+            }
+
+            // Remove existing container->industry links, and replace with
+            // container->transfer unit links
+            for (const transferUnit of transferContainer.producers) {
+                let check = false
+                for (const container of industry.inputs) {
+                    if (isTransferContainerNode(container)) {
+                        continue
+                    }
+                    if (container.item === transferUnit.item) {
+                        container.consumers.delete(industry)
+                        industry.inputs.delete(container)
+                        transferUnit.takeFrom(container)
+                        check = true
+                        break
+                    }
+                }
+                if (!check) {
+                    console.log(industry)
+                    console.log(transferUnit)
+                    throw new Error("Unable to transfer item")
+                }
+            }
+
+            // Link transfer container to industry
+            industry.takeFrom(transferContainer)
+        }
+    }
+}
+
+/**
+ * Sanity check the factory. Check for 1) exceeding container limits,
+ * 2) egress > ingress, 3) or split containers having more than one consumer
+ * @param factory the FactoryGraph
+ */
+function sanityCheck(factory: FactoryGraph): void {
+    // Check containers
+    for (const container of factory.containers) {
+        if (container.incomingLinkCount > MAX_CONTAINER_LINKS) {
+            console.log(container)
+            throw new Error("Container exceeds incoming link limit")
+        }
+        if (container.outgoingLinkCount > MAX_CONTAINER_LINKS) {
+            console.log(container)
+            throw new Error("Container exceeds outgoing link limit")
+        }
+        if (container.egress > container.ingress) {
+            console.log(container)
+            throw new Error("Container egress exceeds ingress")
+        }
+        if (container.isSplit && container.outgoingLinkCount > 1) {
+            console.log(container)
+            throw new Error("Split container has more than one outgoing link")
+        }
+    }
+    // Check industries
+    for (const industry of factory.industries) {
+        if (industry.incomingLinkCount > MAX_INDUSTRY_LINKS) {
+            console.log(industry)
+            throw new Error("Industry exceeds incoming link limit")
+        }
+    }
+}
+
+/**
  * Generate a new factory graph that supplies a given number of assemblers
  * for a given set of products
  * @param requirements Products and number of assemblers
@@ -178,8 +301,8 @@ export function buildFactory(
     /* Add transfer units to relocate byproducts */
     handleByproducts(factory)
     /* Add transfer units and containers to handle industry link limit */
-    factory.handleIndustryLinks()
+    handleIndustryLinks(factory)
     /* Sanity check for errors in factory */
-    factory.sanityCheck()
+    sanityCheck(factory)
     return factory
 }
--- src/graph.ts
+++ src/graph.ts
@@ -17,7 +17,7 @@ import { findRecipe, Recipe } from "./recipes"
 export const MAX_CONTAINER_LINKS = 10
 
 /** Maximum number of incoming industry links */
-const MAX_INDUSTRY_LINKS = 7
+export const MAX_INDUSTRY_LINKS = 7
 
 export type PerMinute = number
 
@@ -598,124 +598,5 @@ export class FactoryGraph {
             Array.from(items).some((item) => !node.items.includes(item)),
         )
         return new Set(transferContainers)
-    }
-
-    /**
-     * Add transfer units and containers to handle industries that require
-     * >7 incoming links
-     */
-    handleIndustryLinks() {
-        // Loop over all factory industries
-        for (const industry of this.industries) {
-            // Get ingredients and ingredient quantities
-            const recipe = findRecipe(industry.item)
-            // Sort ingredients by quantity
-            const recipeIngredients = recipe.ingredients
-            recipeIngredients.sort((a, b) => a.quantity - b.quantity)
-            const ingredients = recipeIngredients.map((ingredient) => ingredient.item)
-
-            // Get exceeding link count
-            const exceedingLinks = industry.incomingLinkCount - MAX_INDUSTRY_LINKS
-            if (exceedingLinks > 0) {
-                let transferContainer: TransferContainerNode | undefined
-
-                // Get all transfer containers containing a subset of the industry ingredients
-                const transferContainers = this.getTransferContainers(new Set(ingredients))
-                for (const checkTransferContainer of transferContainers) {
-                    // Check if this transfer container has at least exceedingLinks+1 items
-                    // +1 because we need to remove one link to make space for TransferContainerNode
-                    if (checkTransferContainer.items.length < exceedingLinks + 1) {
-                        continue
-                    }
-                    // Check if we can add an outgoing link from this TransferContainerNode
-                    if (!checkTransferContainer.canAddOutgoingLinks(1)) {
-                        continue
-                    }
-                    // Check if we can add one ingoing link to each transfer unit on this TransferContainerNode
-                    if (
-                        Array.from(checkTransferContainer.producers).some(
-                            (node) => !node.canAddIncomingLinks(1),
-                        )
-                    ) {
-                        continue
-                    }
-                    // good
-                    transferContainer = checkTransferContainer
-                    break
-                }
-
-                // Create a new TransferContainerNode if necessary
-                if (transferContainer === undefined) {
-                    // Use first exceedingLinks+1 items in ingredients (sorted by quantity)
-                    // +1 because we need to remove one link to make space for TransferContainerNode
-                    const items = ingredients.slice(0, exceedingLinks + 1)
-                    transferContainer = this.createTransferContainer(items)
-                    // Add transfer units
-                    for (const item of items) {
-                        const transferUnit = this.createTransferUnit(item)
-                        transferUnit.outputTo(transferContainer)
-                    }
-                }
-
-                // Remove existing container->industry links, and replace with
-                // container->transfer unit links
-                for (const transferUnit of transferContainer.producers) {
-                    let check = false
-                    for (const container of industry.inputs) {
-                        if (isTransferContainerNode(container)) {
-                            continue
-                        }
-                        if (container.item === transferUnit.item) {
-                            container.consumers.delete(industry)
-                            industry.inputs.delete(container)
-                            transferUnit.takeFrom(container)
-                            check = true
-                            break
-                        }
-                    }
-                    if (!check) {
-                        console.log(industry)
-                        console.log(transferUnit)
-                        throw new Error("Unable to transfer item")
-                    }
-                }
-
-                // Link transfer container to industry
-                industry.takeFrom(transferContainer)
-            }
-        }
-    }
-
-    /**
-     * Sanity check the factory. Check for 1) exceeding container limits,
-     * 2) egress > ingress, 3) or split containers having more than one consumer
-     */
-    sanityCheck() {
-        // Check containers
-        for (const container of this.containers) {
-            if (container.incomingLinkCount > MAX_CONTAINER_LINKS) {
-                console.log(container)
-                throw new Error("Container exceeds incoming link limit")
-            }
-            if (container.outgoingLinkCount > MAX_CONTAINER_LINKS) {
-                console.log(container)
-                throw new Error("Container exceeds outgoing link limit")
-            }
-            if (container.egress > container.ingress) {
-                console.log(container)
-                throw new Error("Container egress exceeds ingress")
-            }
-            if (container.isSplit && container.outgoingLinkCount > 1) {
-                console.log(container)
-                throw new Error("Split container has more than one outgoing link")
-            }
-        }
-        // Check industries
-        for (const industry of this.industries) {
-            if (industry.incomingLinkCount > MAX_INDUSTRY_LINKS) {
-                console.log(industry)
-                throw new Error("Industry exceeds incoming link limit")
-            }
-        }
     }
 }
